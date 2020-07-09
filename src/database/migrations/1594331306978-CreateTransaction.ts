aba54c743d324d38ea6014de1cd3114e6077f3b0
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-09 19:05:11 -0300

    add migrations

---
+++ src/database/migrations/1594331306978-CreateTransaction.ts
@@ -0,0 +1,63 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateTransaction1594331306978 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'transactions',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'title',
+              type: 'varchar',
+            },
+            {
+              name: 'type',
+              type: 'varchar',
+            },
+            {
+              name: 'category_id',
+              type: 'uuid',
+              isNullable: true,
+            },
+            {
+              name: 'value',
+              type: 'numeric',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ]
+        })
+      );
+
+      await queryRunner.createForeignKey('transactions', new TableForeignKey({
+        name: 'transaction_category',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE'
+      }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.dropForeignKey('transactions', 'transaction_category');
+      await queryRunner.dropTable('transactions');
+    }
+
+}
